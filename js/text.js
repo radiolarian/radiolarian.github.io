50f8bb37e863f0ef5d2f9b7855ba81bbc852f53c
Jingyi Li <soundtracknoon@gmail.com>
2016-02-15 00:41:08 -0800

    anime, gpees

--- js/text.js
+++ js/text.js
@@ -7,7 +7,7 @@ context.fill();
 
 var div = document.getElementById("about-text");
 
-div.innerHTML = div.innerHTML + '<p>Hi! I\'m Jingyi, a third year <a href="http://eecs.berkeley.edu">EECS</a> major at UC Berkeley. I really like making things, and helping others do the same.</p>'
+div.innerHTML = div.innerHTML + '<p>Hi! I\'m Jingyi, a third year <a href="http://eecs.berkeley.edu">EECS</a> major at UC Berkeley. I really like making things, and enabling others to do the same.</p>'
 +
 	'<p>You may recognize me from teaching <a href="http://teaching.paulos.net/cs160_FL2015/">CS160:</a> <a href="http://teaching.paulos.net/cs160_SP2016/">UI Design & Development</a>, or from hanging around <a href="http://bid.berkeley.edu">BiD</a>, where I do research under <a href="http://cs.berkeley.edu/~bjoern/">Bjoern Hartmann</a>. Broadly, my research interests are Human-Computer Interaction, Computer Graphics, New Media, and fandom.</p>'
 +
