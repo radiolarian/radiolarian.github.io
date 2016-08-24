d77b93ac2dc297db1d16abe8a34da2e2493f711c
Jingyi Li <noon@berkeley.edu>
2016-08-24 14:25:45 -0700

    change hair color

--- js/text.js
+++ js/text.js
@@ -1,17 +1,15 @@
 var canvas = document.getElementById("circlecanvas");
 var context = canvas.getContext("2d");
 context.arc(90, 90, 190, 0, Math.PI * 2, false);
-context.fillStyle = "#e4267b";
+context.fillStyle = "#ECC266";
 context.fill();
 
 
 var div = document.getElementById("about-text");
 
-div.innerHTML = div.innerHTML + '<p>Hi! I\'m Jingyi, a fourth year <a href="http://eecs.berkeley.edu">EECS</a> major at UC Berkeley. I really like making things, and enabling others to do the same.</p>'
+div.innerHTML = div.innerHTML + '<p>Hi! I\'m Jingyi, a fourth year <a href="http://eecs.berkeley.edu">EECS</a> undergraduate at UC Berkeley. I really like making things, and enabling others to do the same.</p>'
 +
-	'<p>You may recognize me from teaching <a href="http://teaching.paulos.net/cs160_FL2015/">CS160:</a> <a href="http://teaching.paulos.net/cs160_SP2016/">UI Design & Development</a>, or from hanging around <a href="http://bid.berkeley.edu">BiD</a>, where I do research under <a href="http://cs.berkeley.edu/~bjoern/">Bjoern Hartmann</a>. Broadly, my research interests are Human-Computer Interaction, Computer Graphics, New Media, and fandom.</p>'
-+
-	'<p>This summer, I\'m working as a front-end engineering intern at <a href="http://juji.io">Juji</a> and a design consultant at <a href="http://sumupanalytics.com/">SumUp Analytics</a>.</p>'
+	'<p>I currently do research under <a href="http://cs.berkeley.edu/~bjoern/">Bjoern Hartmann</a> in the <a href="http://bid.berkeley.edu">Berkeley Institute of Design</a>, and am teaching <a href="inst.eecs.berkeley.edu/~cs184/fa16">CS 184: Computer Graphics</a>. For the past year, I was also a GSI for <a href="http://teaching.paulos.net/cs160_FL2015/">CS160:</a> <a href="http://teaching.paulos.net/cs160_SP2016/">UI Design & Development</a>. Broadly, my research interests lie within the intersection of digital fabrication and computer graphics, computer vision and computational photography, and fandom.</p>'
 +
 	'<p>The circle in the corner represents my current hair color. Try dragging the drawing of me towards it!</p>';
 
