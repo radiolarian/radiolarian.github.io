4b5b71c9ce75ca1425b96dce462126a8a5fab9d5
Jingyi Li <noon@berkeley.edu>
2017-01-31 00:29:34 -0800

    change hair color, hacky responsiveness fix via hmtl5 summary
    s/o to jeremy for catching large monitor issues

--- js/text.js
+++ js/text.js
@@ -1,17 +1,15 @@
-var canvas = document.getElementById("circlecanvas");
-var context = canvas.getContext("2d");
-context.arc(90, 90, 190, 0, Math.PI * 2, false);
-context.fillStyle = "#2E55B6";
-context.fill();
+var canvas = document.getElementById("circlecanvas"); var context =
+canvas.getContext("2d"); context.arc(90, 90, 190, 0, Math.PI * 2, false);
+context.fillStyle = "#E2739F"; context.fill();
 
 
 var div = document.getElementById("about-text");
 
-div.innerHTML = div.innerHTML + '<p>Hi! I\'m Jingyi, a fourth year <a href="http://eecs.berkeley.edu">EECS</a> undergraduate at UC Berkeley. I really like making things, and enabling others to do the same.</p>'
+div.innerHTML = div.innerHTML + '<p>Hi! I\'m Jingyi, a recent <a href="http://eecs.berkeley.edu">EECS</a> graduate from UC Berkeley. I really like making things, and enabling others to do the same.</p>'
 +
-	'<p>I currently do research under <a href="http://cs.berkeley.edu/~bjoern/">Bjoern Hartmann</a> in the <a href="http://bid.berkeley.edu">Berkeley Institute of Design</a>, and am teaching <a href="inst.eecs.berkeley.edu/~cs184/fa16">CS 184: Computer Graphics</a>. For the past year, I also taught <a href="http://teaching.paulos.net/cs160_FL2015/">CS160:</a> <a href="http://teaching.paulos.net/cs160_SP2016/">UI Design</a>. Broadly, my research interests include design tools for digital fabrication, some intersection of computer graphics, vision, and computational photography, and fan cultures.</p>'
+	'<p>At Berkeley, I do research under <a href="http://cs.berkeley.edu/~bjoern/">Bjoern Hartmann</a> in the <a href="http://bid.berkeley.edu">Berkeley Institute of Design</a>. For the past three semesters, I\'ve taught <a href="inst.eecs.berkeley.edu/~cs184/fa16">CS 184: Computer Graphics</a>, and <a href="http://teaching.paulos.net/cs160_FL2015/">CS160:</a> <a href="http://teaching.paulos.net/cs160_SP2016/">UI Design</a>. Broadly, my research interests include design tools for digital fabrication, some intersection of computer graphics, vision, and computational photography, and fan cultures.</p>'
 +
-	'<p>I\'m graduating a semester early to take a design internship at <a href="http://nvidia.com">NVIDIA</a>. After that, I hope to continue my research through a PhD program in HCI! I use they/them pronouns.</p>'
+	'<p>I graduated a semester early to intern as a UI/UX designer at <a href="http://nvidia.com">NVIDIA</a>. After that, I hope to continue my research through a PhD program in HCI! I use they/them pronouns.</p>'
 
 +
 	'<p>The circle in the corner represents my current hair color. Try dragging the drawing of me towards it!</p>';
