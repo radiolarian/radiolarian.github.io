5cce6a1de5854a1f46f0f438c83ba7eaaaa914c7
radiolarian <noon@berkeley.edu>
2019-01-10 18:54:03 -0800

    add new site

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// for kerning
+    $(document).ready(function() {
+        $(".name").lettering();
+        $(".lastname").lettering();
+    
+        //for hair
+        var isColored = false;
+        var color1 = '#574fbf';
+        var color2 = '#c1b9d7';
+        var color3 = '#e05bbf';
+        var msg = $(".last-par").html();
+
+        $(".last-par").on('click', '#change-hair', function() {
+            console.log("hi");
+            isColored = (isColored ? false : true);
+            console.log(isColored);
+            if (isColored) {
+                $('.name > span').each(function () { $(this).css('color', color1); });
+                $(".name > .char2").css('color', color2);
+                $(".name > .char3").css('color', color2);
+                $(".name > .char5").css('color', color3);
+                $(".name > .char6").css('color', color2);
+                $('.lastname > span').each(function () { $(this).css('color', color3); });
+                $(".lastname > .char2").css('color', color1);
+            }
+            else {
+                $('.name > span').each(function () { $(this).css('color', '#333333'); });
+                $('.lastname > span').each(function () { $(this).css('color', '#333333'); });
+            }
+
+            //add text on small viewports
+            var width = $(window).width();
+            if (width <= 1024) {
+                console.log("small port");
+                $(".last-par").html(msg + " You need to expand the sidebar to see the changes. ");
+            } else {
+                console.log("normal");
+                $(".last-par").html(msg);
+            }
+
+            return false;
+        });
+
+    });
---
+++ starcollector/js/screens/play.js
@@ -0,0 +1,72 @@
+game.PlayScreen = me.ScreenObject.extend({
+	/**
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {
+		//load level
+		me.levelDirector.loadLevel("spring");
+
+		game.data.score = 0;
+		game.data.numCollected = 0;
+		game.data.textBox = "";
+
+		//music
+		me.audio.playTrack("spring");
+
+		// add our HUD to the game world
+		this.HUD = new game.HUD.Container();
+		me.game.world.addChild(this.HUD);
+		//reset a level and mute
+		me.input.bindKey(me.input.KEY.R, "reset", true);
+		me.input.bindKey(me.input.KEY.M, "mute", true);
+        this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge) {
+            if (action === "reset") {
+            	game.data.numCollected = 0;
+            	game.data.score = 0;
+                me.levelDirector.reloadLevel();
+                me.audio.stopTrack();
+	                if (!game.data.mute) {
+		                if (game.data.level == "SPRING")
+		                	me.audio.playTrack("spring");
+		                if (game.data.level == "SUMMER")
+		                	me.audio.playTrack("summer");
+		                if (game.data.level == "FALL" || game.data.level == "FALL2")
+		                	me.audio.playTrack("fall");                
+		                if (game.data.level == "WINTER" || game.data.level == "WINTER2")
+		                	me.audio.playTrack("winter");
+     	           }
+            }
+            if (action === "mute") {
+                if (!game.data.mute) {
+                	me.audio.stopTrack();
+                	game.data.mute = true;
+                }
+                else if (game.data.mute) {
+	                if (game.data.level == "SPRING")
+	                	me.audio.playTrack("spring");
+	                if (game.data.level == "SUMMER")
+	                	me.audio.playTrack("summer");
+	                if (game.data.level == "FALL" || game.data.level == "FALL2")
+	                	me.audio.playTrack("fall");                
+	                if (game.data.level == "WINTER" || game.data.level == "WINTER2")
+	                	me.audio.playTrack("winter");                	
+	                game.data.mute = false;
+                }
+            }            
+        });		
+	},
+
+
+	/**
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		//stop music
+		me.audio.stopTrack();
+		// remove the HUD from the game world
+		me.game.world.removeChild(this.HUD);
+		// unbind keys
+		me.input.unbindKey(me.input.KEY.R);
+		me.input.unbindKey(me.input.KEY.M);
+	}
+});
